d9e82d576856c22a3262aa89d8f839116f28da9a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-08 09:33:15 +0430

    add EventListener to btn

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector("color");
 
+btn.addEventListener('click', function () {
+    const randomNumber = 2
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
