6291660aa4038c480f4f3d80c069ee8eac2ea119
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-08 11:00:33 +0430

    add for each

--- Counter-HTML/app.js
+++ Counter-HTML/app.js
@@ -1,2 +1,19 @@
 const value = document.getElementById('value')
 const btns = document.querySelectorAll(".btn")
+
+let count = 0;
+
+btns.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        const style = e.currentTarget.classList
+        console.log(style)
+        if (style.contains("increase")) {
+            count++;
+        } else if (style.contains("decrease")) {
+            count--;
+        } else {
+            count = 0;
+        }
+        value.textContent = count
+    })
+})
