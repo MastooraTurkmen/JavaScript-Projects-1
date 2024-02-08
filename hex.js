f57109e88f419313b6787a1bc20ab5fee68720ca
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-08 10:06:46 +0430

    set up hex js

--- hex.js
+++ hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click", function () {
+    let hexCode = "#";
+    for (let i = 0; i < 6; i++) {
+        hexCode += hex[getRandomeNumber()]
+    }
+
+    document.body.style.backgroundColor = hexCode;
+    color.textContent = hexCode;
+})
+
+function getRandomeNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
