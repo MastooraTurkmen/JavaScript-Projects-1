d152615d56664dee52b7b07353a416bf5622b91a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-09 08:16:01 +0430

    set up app js

--- 4. Navbar/app.js
+++ 4. Navbar/app.js
@@ -3,3 +3,20 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navbar = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+
+navbar.addEventListener('click', function () {
+    if (links.classList.contains("show-links")) {
+        links.classList.remove("show-links")
+    } else {
+        links.classList.add("show-links")
+    }
+    
+    /*
+    we can also use this methode, too.
+      links.classList.toggle("show-links")
+    */
+})
