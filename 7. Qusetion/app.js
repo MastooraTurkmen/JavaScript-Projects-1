ada20c603da74d92707e115877aec4cf9639516d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-10 10:54:53 +0430

    adds for each method

--- 7. Qusetion/app.js
+++ 7. Qusetion/app.js
@@ -7,7 +7,14 @@ const questions = document.querySelectorAll(".question");
 questions.forEach(function (question) {
     const btn = question.querySelector(".question-btn")
     btn.addEventListener("click", function () {
-        console.log(question.classList.toggle("show-text"))
+
+        questions.forEach(function (item) {
+            if (item !== question) {
+                item.classList.remove("show-text")
+            }
+        })
+
+        question.classList.toggle("show-text")
     })
 })
 
