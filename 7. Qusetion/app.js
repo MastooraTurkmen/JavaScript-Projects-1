89e7c23f27fc5e7587b6299599e7b444017d19c1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-10 10:47:27 +0430

    setup two methods

--- 7. Qusetion/app.js
+++ 7. Qusetion/app.js
@@ -1,13 +1,31 @@
-//using selectors inside the element
-// traversing the dom
+/*
+First Method: using selectors inside the element
+*/
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", function () {
+        console.log(question.classList.toggle("show-text"))
+    })
+})
 
 
-const btns = document.querySelectorAll(".question-btn");
 
 
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle("show-text")
-    });
-});
+
+
+
+/*
+Second Method: traversing the dom
+*/
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text")
+//     });
+// });
