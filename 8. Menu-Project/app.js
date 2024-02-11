d8c25e95f95b383c01a316e69549cee488da861a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-11 17:14:59 +0430

    adds more feature

--- 8. Menu-Project/app.js
+++ 8. Menu-Project/app.js
@@ -71,6 +71,14 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "quarantine buddy",
+    category: "dinner",
+    price: 36.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
