d887895aaea3b36ba474b31733b6f9929d8d67ab
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-14 16:14:55 +0430

    addEventListener

--- 14. GroceryBud/app.js
+++ 14. GroceryBud/app.js
@@ -1,8 +1,36 @@
 // ****** SELECT ITEMS **********
 
+const alet = document.querySelector(".alert")
+const groceryContainer = document.querySelector(".grocery-container")
+const form = document.querySelector(".grocery-form");
+const list = document.querySelector(".grocery-list");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn")
+const clearBtn = document.querySelector(".clear-btn")
+
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+form.addEventListener("submit", addItem);
+
+function addItem(e) {
+    e.preventDefault();
+
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if (value !== '' && editFlag === false) {
+        console.log("add a file to list")
+    } else if (value !== '' && editFlag === true) {
+        console.log("editing")
+    } else {
+        console.log("empty list")
+    }
+
+}
 
 // ****** FUNCTIONS **********
 
