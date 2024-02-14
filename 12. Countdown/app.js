362fb80e155342a29b55e97b27f2731c3a8a69d4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-14 09:01:04 +0430

    set up time

--- 12. Countdown/app.js
+++ 12. Countdown/app.js
@@ -24,7 +24,7 @@ const weekdays = [
 
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
-const deadlineFormat = document.querySelectorAll(".deadline-format");
+const deadlineFormat = document.querySelectorAll(".deadline-format h4");
 
 
 const futureDate = new Date(2024, 2, 25, 11, 30, 0);
@@ -38,4 +38,46 @@ const month = months[futureDate.getMonth()];
 const day = weekdays[futureDate.getDay()];
 
 
-giveaway.innerHTML = `giveaway ends on ${day}, ${date} ${month} ${year}, ${hours}: ${minutes} am`
+giveaway.innerHTML = `giveaway ends on ${day}, ${date} ${month} ${year}, ${hours}: ${minutes}`
+
+
+
+function displayDate() {
+  const today = new Date().getTime();
+  const t = futureDate - today;
+
+  // 1sec = 1000ms
+  // 1min = 1000 * 60
+  // 1hour = 60 * 60 * 1000
+  // 1day = 24 * 60 * 60 * 1000
+
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 1000 * 60;
+
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array;
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`)
+    } else {
+      return item;
+    }
+  }
+
+  deadlineFormat.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  })
+
+}
+
+displayDate();
